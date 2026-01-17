462619ee8a16e6fdf69f9766963d3e6335e12010
idan <idandrori555@gmail.com>
2026-01-17 16:42:12 +0200

    finished first part

---
+++ ex10-cs.cpp
@@ -0,0 +1,87 @@
+#include <ctime>
+#include <stdio.h>
+#include <windows.h>
+
+constexpr int PHILOSOPHERS_COUNT = 5;
+constexpr int MEALS_PER_PHILOSOPHER = 1000000;
+
+template <class T>
+constexpr inline T min(T a, T b)
+{
+  return a < b ? a : b;
+}
+
+template <class T>
+constexpr inline T max(T a, T b)
+{
+  return a > b ? a : b;
+}
+
+static CRITICAL_SECTION sticks[PHILOSOPHERS_COUNT];
+
+struct PhilosopherData
+{
+  int id;
+};
+
+DWORD WINAPI tryEat(LPVOID lpParameter)
+{
+  PhilosopherData *pData = (PhilosopherData *)lpParameter;
+  int id = pData->id;
+
+  int left = id;
+  int right = (id + 1) % PHILOSOPHERS_COUNT;
+
+  int first = min(left, right);
+  int second = max(left, right);
+
+  for (int i = 0; i < MEALS_PER_PHILOSOPHER; ++i)
+  {
+    EnterCriticalSection(&sticks[first]);
+    EnterCriticalSection(&sticks[second]);
+
+    LeaveCriticalSection(&sticks[second]);
+    LeaveCriticalSection(&sticks[first]);
+  }
+
+  return 0;
+}
+
+int main(void)
+{
+  HANDLE threads[PHILOSOPHERS_COUNT];
+  PhilosopherData pData[PHILOSOPHERS_COUNT];
+
+  for (int i = 0; i < PHILOSOPHERS_COUNT; ++i)
+  {
+    InitializeCriticalSection(&sticks[i]);
+  }
+
+  puts("Starting to measure time...");
+  clock_t start = clock();
+
+  for (int i = 0; i < PHILOSOPHERS_COUNT; ++i)
+  {
+    pData[i].id = i;
+    threads[i] = CreateThread(NULL, 0, tryEat, &pData[i], 0, NULL);
+    if (!threads[i])
+    {
+      printf("CreateThread failed\n");
+      return 1;
+    }
+  }
+
+  WaitForMultipleObjects(PHILOSOPHERS_COUNT, threads, TRUE, INFINITE);
+
+  clock_t end = clock();
+  double totalTime = (double)(end - start) / CLOCKS_PER_SEC;
+  printf("Time taken: %f seconds\n", totalTime);
+
+  for (int i = 0; i < PHILOSOPHERS_COUNT; ++i)
+  {
+    CloseHandle(threads[i]);
+    DeleteCriticalSection(&sticks[i]);
+  }
+
+  return 0;
+}
