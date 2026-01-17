431ba8a0780ee270b3900253315a2108db6a87dc
idan <idandrori555@gmail.com>
2026-01-17 14:36:21 +0200

    Added HW file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <windows.h>
 
+DWORD WINAPI func(LPVOID lparam)
+{
+  printf("Hello From thread\n");
+  return 0;
+}
+
 int main(void)
 {
+  char command[] = "test.exe";
+  STARTUPINFOA si = {sizeof(si)};
+  PROCESS_INFORMATION pi = {0};
+
+  if (CreateProcessA(NULL, command, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+  {
+    printf("Process created\n");
+    CloseHandle(pi.hThread);
+    CloseHandle(pi.hProcess);
+  }
+
   return 0;
 }
